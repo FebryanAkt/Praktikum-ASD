6a503fe61091c46fcdb1cf0947571eb403be8309
FebryanAkt <febryan08022006@gmail.com>
2025-02-20 09:43:19 +0700

    Jawaban no 4 percobaan 1

--- Jobsheet2/Mahasiswa10.java
+++ Jobsheet2/Mahasiswa10.java
@@ -22,6 +22,9 @@ public class Mahasiswa10 {
         kelas = kelasBaru;
     }
     void updateIpk(double ipkBaru) {
+        if (ipk <= 0.0 || ipk <= 4.0) {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
         ipk = ipkBaru;
     }
     String nilaiKinerja() {
