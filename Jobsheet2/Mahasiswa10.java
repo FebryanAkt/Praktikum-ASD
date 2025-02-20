b286510f4051a69f980fa8527d538cc915e69412
FebryanAkt <febryan08022006@gmail.com>
2025-02-20 08:48:46 +0700

    Percobaan 1

---
+++ Jobsheet2/Mahasiswa10.java
@@ -0,0 +1,30 @@
+public class Mahasiswa10 {
+    String nim;
+    String kelas;
+    double ipk;
+    String nama;
+    
+    void tampilkanIformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: "+ nim);
+        System.out.println("IPK: "+ ipk);
+        System.out.println("Kelas: "+ kelas);
+    }
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+    void updateIpk(double ipkBaru) {
+        ipk = ipkBaru;
+    }
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+}
