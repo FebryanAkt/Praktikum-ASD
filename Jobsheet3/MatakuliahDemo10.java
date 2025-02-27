6bee284e6369f365747b4df3b1af17d767b16182
FebryanAkt <febryan08022006@gmail.com>
2025-02-27 13:47:50 +0700

    Pertanyaan percobaan 3 no 4

--- Jobsheet3/MatakuliahDemo10.java
+++ Jobsheet3/MatakuliahDemo10.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 public class MatakuliahDemo10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Matakuliah10[] arrayOfMatakuliah = new Matakuliah10[3];
+        System.out.print("Masukkan jumlah matakuliah: ");
+        int jmlMk = sc.nextInt();
+        sc.nextLine();
+        Matakuliah10[] arrayOfMatakuliah = new Matakuliah10[jmlMk];
+        
         for (int i = 0; i < arrayOfMatakuliah.length; i++) {
             System.out.println("Masukkan Data Matakuliah ke-" + (i+1));
             arrayOfMatakuliah[i] = new Matakuliah10("", "", 0, 0);
