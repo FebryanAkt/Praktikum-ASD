d720afd313706587bd1856b92d4c6c6f1e6c2d22
FebryanAkt <febryan08022006@gmail.com>
2025-03-26 09:44:12 +0700

    praktikum 2 sorting ipk bubble sort

---
+++ Jobsheet6/Mahasiswa10.java
@@ -0,0 +1,22 @@
+public class Mahasiswa10 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    Mahasiswa10(){
+    }
+    
+    Mahasiswa10 (String nm, String name, String kls, double ip){
+        nim = nm;
+        nama = name;
+        ipk = ip;
+        kelas = kls;
+    }
+    void tampilInformasi(){
+        System.out.println("Nama: "+ nama);
+        System.out.println("NIM: "+ nim);
+        System.out.println("IPK: "+ ipk);
+        System.out.println("Kelas: "+ kelas);
+    }
+}
