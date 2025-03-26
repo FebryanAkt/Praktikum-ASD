e1730f8b8544199f263d28ff3114517d3f32481e
FebryanAkt <febryan08022006@gmail.com>
2025-03-26 12:00:15 +0700

    praktikum 2 sorting ipk selection sort

--- Jobsheet6/MahasiswaDemo10.java
+++ Jobsheet6/MahasiswaDemo10.java
@@ -5,15 +5,15 @@ public class MahasiswaDemo10 {
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < 5; i++) {
             System.out.println("Masukkan data mahasiswa ke-" + (i + 1));
-            System.out.print("Nama: ");
-            String nama = sc.nextLine();
             System.out.print("NIM: ");
             String nim = sc.nextLine();
+            System.out.print("Nama: ");
+            String nama = sc.nextLine();
+            System.out.print("Kelas: ");
+            String kelas = sc.nextLine();
             System.out.print("IPK: ");
             double ipk = sc.nextDouble();
             sc.nextLine(); 
-            System.out.print("Kelas: ");
-            String kelas = sc.nextLine();
             
             list.tambah(new Mahasiswa10(nim, nama, kelas, ipk));
         }
@@ -22,6 +22,10 @@ public class MahasiswaDemo10 {
 
         System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC): ");
         list.bubbleSort();
+        list.tampil();
+
+        System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC)");
+        list.selectionSort();
         list.tampil();
     }
 }
