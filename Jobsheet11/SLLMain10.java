875b6dd73df9f41d9d3427ec4d4ab4eb4e8e1e3c
FebryanAkt <febryan08022006@gmail.com>
2025-05-28 06:05:49 +0700

    Pertanyaan percobaan 1

--- Jobsheet11/SLLMain10.java
+++ Jobsheet11/SLLMain10.java
@@ -1,19 +1,30 @@
+import java.util.Scanner;
 public class SLLMain10 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
         SingleLinkedList10 sll = new SingleLinkedList10();
 
-        Mahasiswa10 mhs1 = new Mahasiswa10("24212200","Alvaro","1A",4.0);
-        Mahasiswa10 mhs2 = new Mahasiswa10("23212201","Bimon","2B",3.8);
-        Mahasiswa10 mhs3 = new Mahasiswa10("22212202","Cintia","3C",3.5);
-        Mahasiswa10 mhs4 = new Mahasiswa10("21212203","Dirga","4D",3.6);
+        System.out.print("Masukkan jumlah data: ");
+        int input = sc.nextInt();
+        sc.nextLine();
 
-        sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addLast(mhs1);
-        sll.print();
-        sll.insertAfter("Dirga", mhs3);
-        sll.insertAt(2, mhs2);
-        sll.print();
+        for (int i = 0; i < input; i++) {
+             System.out.println("Data Mahasiswa ke-" + (i+1));
+            System.out.print("NIM: ");
+            String nim = sc.nextLine();
+            System.out.print("Nama: ");
+            String nama = sc.nextLine();
+            System.out.print("Kelas: ");
+            String kelas = sc.nextLine();
+            System.out.print("IPK: ");
+            double ipk = sc.nextDouble();
+            sc.nextLine(); 
+
+            Mahasiswa10 mhs = new Mahasiswa10(nim, nama, kelas, ipk);
+            sll.addLast(mhs);
+            System.out.println("Setelah penambahan:");
+            sll.print();
+        }
+        sc.close();
     }
 }
